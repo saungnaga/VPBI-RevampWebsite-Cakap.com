83f9ab2f1cafcd1015a57101d7bccbc149c77f95
anugrahp13 <anugrahprastyo13@gmail.com>
2024-11-23 21:23:10 +0700

    feat[circle-button]: added components circle-button

--- src/components/atoms/circle-button.tsx
+++ src/components/atoms/circle-button.tsx
@@ -1,7 +1,26 @@
-// Circle Button
+type CircleButtonProps = {
+  icon?: React.ElementType;
+  variant: "blue" | "black";
+  className?: string;
+  iconClassName?: string;
+};
 
-import React from "react";
-
-export const CircleButton = () => {
-  return <div>CircleButton</div>;
+export const Sosmed: React.FC<CircleButtonProps> = ({
+  icon: Icon,
+  variant,
+  className = "",
+  iconClassName = "",
+}) => {
+  const defaultStyle =
+    "w-10 h-10 rounded-full flex justify-center items-center";
+  const variants = {
+    blue: "border border-[#00ADC6] hover:border-[#00ADC6] text-white hover:bg-[#00ADC6] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
+    black:
+      "border border-[#000000] hover:border-[#000000] text-white hover:bg-[#000000] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
+  };
+  return (
+    <button className={`${defaultStyle} ${variants[variant]} ${className}`}>
+      {Icon && <Icon className={iconClassName} />}
+    </button>
+  );
 };
