8582cbe3456f47827206c0403b6f4934fe00f058
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-11-23 23:34:23 +0700

    feat [atom-component]: added accordion component

--- src/components/atoms/accordion.tsx
+++ src/components/atoms/accordion.tsx
@@ -1,7 +1,72 @@
-//accordion
+// accordion
 
-import React from "react";
+/**
+  
+  required data structure!
 
-export const accordion = () => {
-  return <div>accordion</div>;
+  [
+    {
+      id: "1",
+      title: "Section 1",
+      content: "Content for Section 1",
+    }
+  ];
+
+ */
+
+"use client";
+import React, { useState } from "react";
+import { IAccordionPropTypes } from "./types/accordion.types";
+
+export const Accordion: React.FC<IAccordionPropTypes> = ({
+  allowMultipleOpen = false,
+  items = [
+    {
+      id: "item1",
+      title: "Accordion Item 1",
+      content: "This is the content for Accordion Item 1.",
+    },
+  ],
+}) => {
+  const [open, setOpen] = useState<string[]>([]);
+
+  const toggleAccordion = (id: string) => {
+    if (allowMultipleOpen) {
+      setOpen((prev) =>
+        prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+      );
+    } else {
+      setOpen((prev) => (prev.includes(id) ? [] : [id]));
+    }
+  };
+
+  return (
+    <>
+      {items.map(({ id, title, content }) => (
+        <div key={id} className={`rounded `}>
+          <button
+            className={`w-full flex justify-between items-center p-4 bg-white hover:bg-slate-100 border-b-2 `}
+            onClick={() => toggleAccordion(id)}
+          >
+            <span
+              className={`font-semibold text-gray-700 ${
+                open.includes(id) && "text-[#00ADC6]"
+              } transition-all duration-500`}
+            >
+              {title}
+            </span>
+            <span>+</span>
+          </button>
+          <div
+            id={`accordion-content-${id}`}
+            className={`overflow-hidden duration-500 ${
+              open.includes(id) ? "max-h-96" : "max-h-0"
+            }`}
+          >
+            <div className="p-4 text-gray-600 text-sm">{content}</div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
---
+++ src/components/atoms/types/accordion.types.ts
@@ -0,0 +1,10 @@
+export interface IAccordionItemTypes {
+    id: string;
+    title: string;
+    content: string;
+}
+
+export interface IAccordionPropTypes {
+    items: IAccordionItemTypes[];
+    allowMultipleOpen?: boolean;
+}
