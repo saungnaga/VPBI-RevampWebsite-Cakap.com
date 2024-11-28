584f9935059b609fb03233839a74c8d9596ac2c8
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-11-28 16:21:24 +0700

    refactor [services/faq-api.ts]: added null safety in a response

--- src/services/faq-api.ts
+++ src/services/faq-api.ts
@@ -2,5 +2,5 @@ import { apiInstance } from "@/config/axiosInstance";
 
 export const getFaqs = async () => {
     const response = await apiInstance.get('/selfpaced/faq?tenant=cakap');
-    return response.data;
+    return response?.data;
 };
