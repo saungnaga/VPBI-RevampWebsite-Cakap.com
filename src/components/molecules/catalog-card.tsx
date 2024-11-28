d6db5e5d17f17ae3664b768aa7147c6113ea78b2
prasetyodimasp <prasetyodimasp2711@gmail.com>
2024-11-28 20:51:52 +0700

    feat [molecules -component] added component catalog-card

--- src/components/molecules/catalog-card.tsx
+++ src/components/molecules/catalog-card.tsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { CatalogCardProps } from "./types/catalog-card.types";
 
-export const CatalogCard = () => {
-  return <div>CatalogCard</div>;
+export const CatalogCard: React.FC<CatalogCardProps> = ({
+    data,
+    className,
+}) => {
+    return (
+        <div
+            className={`relative w-96 h-52 bg-gray-300 rounded-xl overflow-hidden ${className}`}
+        >
+            <div className="absolute top-0 right-0 bg-gray-500 text-white px-12 py-2 rounded-bl-xl">
+                {data.discount}
+            </div>
+
+            <div className="absolute bottom-0 left-0 p-6 text-white">
+                <div className="text-2xl font-semibold">{data.title}</div>
+                <div className="text-lg">{data.mentor}</div>
+            </div>
+
+            <div className="absolute bottom-0 right-0 p-6 text-white">
+                <div className="text-2xl font-semibold">{data.price}</div>
+            </div>
+        </div>
+    );
 };
+
+export default CatalogCard;
---
+++ src/components/molecules/types/catalog-card.types.ts
@@ -0,0 +1,9 @@
+export interface CatalogCardProps {
+    data: {
+        discount: string;
+        title: string;
+        mentor: string;
+        price: string;
+    };
+    className?: string;
+}
