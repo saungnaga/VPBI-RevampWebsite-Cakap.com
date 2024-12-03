e3c7ff40fd328fce3e09316c92e8fac40d8e41e7
saungnaga <saungnagaa@gmail.com>
2024-12-03 11:28:10 +0700

    refactor [circleButton]: change label to children

--- src/components/atoms/circle-button.tsx
+++ src/components/atoms/circle-button.tsx
@@ -1,21 +1,20 @@
 import { ICircleButtonPropTypes } from "./types/circle-button.types";
 
-export const Sosmed: React.FC<ICircleButtonPropTypes> = ({
-  icon: Icon,
+export const CircleButton: React.FC<ICircleButtonPropTypes> = ({
+  children,
   variant,
   className = "",
-  iconClassName = "",
 }) => {
   const defaultStyle =
-    "w-10 h-10 rounded-full flex justify-center items-center";
+    "p-1 rounded-full flex justify-center items-center";
   const variants = {
-    blue: "border border-[#00ADC6] hover:border-[#00ADC6] text-white hover:bg-[#00ADC6] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
+    blue: "border border-[#00ADC6] hover:border-[#00ADC6] text-white bg-[#00ADC6] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
     black:
-      "border border-[#000000] hover:border-[#000000] text-white hover:bg-[#000000] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
+      "border border-[#000000] hover:border-[#000000] text-white bg-[#000000] hover:shadow-lg hover:opacity-80 transition duration-300 ease-in-out",
   };
   return (
     <button className={`${defaultStyle} ${variants[variant]} ${className}`}>
-      {Icon && <Icon className={iconClassName} />}
+      {children}
     </button>
   );
 };
--- src/components/atoms/types/circle-button.types.ts
+++ src/components/atoms/types/circle-button.types.ts
@@ -1,6 +1,5 @@
 export interface ICircleButtonPropTypes {
-    icon?: React.ElementType;
     variant: "blue" | "black";
     className?: string;
-    iconClassName?: string;
+    children: React.ReactNode;
 };
