0fafd910649b704bff2e11da4e92522d6700a295
saungnaga <saungnagaa@gmail.com>
2024-12-03 10:42:07 +0700

    feat [homepage]: create basic layout for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,96 @@
 //Homepage
 
-import Image from "next/image";
+import { Accordion } from "@/components/atoms/accordion";
+import Badge from "@/components/atoms/badge";
+import { Sosmed } from "@/components/atoms/circle-button";
+import { Header } from "@/components/molecules/header";
+import JobList from "@/components/molecules/job-list";
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div
-          className="flex gap
-        -4 items-center flex-col sm:flex-row"
-        >
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <h1 className="hidden">
+        Kursus Online Bersertifikat dan Kelas Keterampilan Kerja - Cakap
+      </h1>
+      <Header />
+      {/* -----banner----- */}
+      <div className="lg:w-1/2 md:px-32 sm:px-20 p-12 flex flex-col gap-4">
+        <div className="text-4xl font-extrabold">
+          Raih <span className="text-[#00ADC6]">tujuan</span> dan{" "}
+          <span className="text-[#00ADC6]">sasaran Anda</span> di masa mendatang
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      {/* -------course preview------- */}
+      <div className="lg:px-32 md:px-20 p-12 bg-[#E1EDF7]">
+        <div className="text-center">
+          <div className="text-xl lg:text-4xl md:text-3xl sm:text-2xl font-extrabold">Jelajahi Dua Cara Belajar Terbaik</div>
+          <div className="mt-2 text-sm sm:text-base">
+            Belajar langsung dari pakarnya dengan live webinar atau atur sendiri ritme belajar Anda dengan kursus mandiri.
+          </div>
+        </div>
+      </div>
+      {/* -------Kategori-------- */}
+      <div className="lg:px-32 md:px-20 p-12">
+        <div className="text-3xl font-extrabold">
+          Beragam <span className="text-[#00ADC6]">Kategori</span>,
+        </div>
+        <div className="text-3xl font-extrabold">Tak Terbatas Kesempatan</div>
+      </div>
+      {/* -------Kursus Terlaris------ */}
+      <div className="lg:px-32 md:px-20 p-12 bg-[#E1EDF7]">
+        <div className="text-center">
+          <div className="text-xl lg:text-4xl md:text-3xl sm:text-2xl font-extrabold">Jangan Lewatkan, Ini yang Paling Dicari!</div>
+          <div className="mt-2 text-sm sm:text-base">
+            Daftar sekarang untuk bergabung dengan kursus terbaik kami.
+          </div>
+        </div>
+      </div>
+      {/* -------Bahasa Asing-------- */}
+      <div className="lg:px-32 md:px-20 p-12">
+        <div className="text-3xl font-extrabold">Kelas Live Bahasa Asing</div>
+        <div className="text-3xl font-extrabold">
+          di <span className="text-[#00ADC6]">Cakap Club</span>,
+        </div>
+      </div>
+      {/* -------Karir-------- */}
+      <div className="lg:px-32 md:px-20 p-12 bg-[#E1EDF7] flex flex-col gap-10 md:flex-row items-center">
+        <div className="flex flex-col justify-center gap-5 lg:w-1/2 md:w-1/3 lg:p-16">
+          <div className="text-4xl font-extrabold">
+            Temukan <div className="text-[#00ADC6]">Peluang Karier</div> Impian Anda
+          </div>
+          <div>
+            <div className="text-lg mb-5">
+            Jelajahi berbagai 
+            lowongan pekerjaan yang sesuai dengan keahlian dan minat Anda.
+            </div>
+            <Badge label="Lihat Semua ↗" link="" bgcolor="white"/>
+          </div>
+        </div>
+        <JobList />
+      </div>
+      {/* --------Prakerja------- */}
+      <div className="lg:px-32 md:px-20 p-12 bg-[#00ADC6]">
+          <div className="text-2xl lg:text-4xl sm:text-3xl font-extrabold text-center text-white">Cakap X Prakerja</div>
+      </div>
+      {/* --------Mitra-------- */}
+      <div className="lg:px-32 md:px-20 p-12 border-b-2 border-black shadow-md">
+        <div className="text-2xl lg:text-4xl sm:text-3xl font-extrabold text-center">Mitra Kursus Kami</div>
+      </div>
+      {/* --------FAQ--------- */}
+      <div className="lg:px-32 md:px-20 p-12 flex flex-col md:flex-row md:gap-10 gap-2 text-center md:text-left">
+        <div className="flex flex-col justify-between md:w-1/2">
+          <div className="text-xl lg:text-4xl sm:text-3xl font-extrabold">
+            <span className="text-yellow-500">FAQ</span>: Jawaban Cepat untuk Anda
+          </div>
+          <div className="text-sm sm:text-base">
+            Cari tahu segala hal yang Anda butuhkan dengan mudah
+          </div>
+        </div>
+        <div>
+          <Accordion />
+        </div>
+      </div>
+    </>
   );
 }
