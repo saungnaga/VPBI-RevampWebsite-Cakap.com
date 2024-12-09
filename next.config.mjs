845b1eeca6da51ab96d8ce0a00d6ce57c4ff1fb0
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-09 21:49:24 +0700

    chore [next.config]: added image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["s3.ap-southeast-3.amazonaws.com", "cdn.cursor-trails.com"],
+  },
+};
 
 export default nextConfig;
