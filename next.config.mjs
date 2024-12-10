c5bd2af9bb884a1a2ecfe3bdadd5b9fbb7fa9104
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-10 16:08:08 +0700

    chore [next.config]: added image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["s3.ap-southeast-3.amazonaws.com", "cdn.cursor-trails.com"],
+    domains: [
+      "s3.ap-southeast-3.amazonaws.com",
+      "s3.ap-southeast-1.amazonaws.com",
+      "cdn.cursor-trails.com",
+    ],
   },
 };
 
