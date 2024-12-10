dc0963eeea8771e8226d62c7d893c9016ba89161
zeinirfansyah <12.zeinirfansyah@gmail.com>
2024-12-10 16:11:26 +0700

    refactor [catalog-card]: fixed card layouts

--- src/components/molecules/catalog-card.tsx
+++ src/components/molecules/catalog-card.tsx
@@ -17,7 +17,7 @@ export const CatalogCard: React.FC<CatalogCardProps> = ({
         className="shadow-md rounded-xl hover:shadow-md "
       >
         <picture className="relative">
-          <div className="bg-gradient-to-b from-[#00000000] to-[#000000e9] absolute inset-0 z-10 rounded-xl backdrop-brightness-100 hover:backdrop-brightness-75 transition-all duration-300"></div>
+          <div className="bg-gradient-to-b from-[#00000000] to-[#000000e1] absolute inset-0 z-10 rounded-xl backdrop-brightness-100 hover:backdrop-brightness-75 transition-all duration-300"></div>
           {thumbnail ? (
             <Image
               src={thumbnail}
@@ -33,25 +33,36 @@ export const CatalogCard: React.FC<CatalogCardProps> = ({
               width="0"
               height="0"
               sizes="100vw"
-              alt={thumbnail}
+              alt={title}
+              priority
               className="rounded-xl object-cover h-60 w-full transition duration-500"
             />
           )}
           <div className="absolute right-0 top-0 rounded-bl-xl rounded-tr-xl">
             <div className="flex items-center">
-              <p className="text-white text-sm font-semibold bg-primary px-3 py-1 w-fit rounded-bl-xl rounded-tr-xl capitalize">
-                {title}
-              </p>
-              title
+              {discount && (
+                <p className="text-white text-sm text-center font-semibold bg-primary px-3 py-1 min-w-[140px] rounded-bl-xl rounded-tr-xl capitalize bg-yellow-500">
+                  Disc {discount}
+                </p>
+              )}
             </div>
           </div>
           <div className="absolute left-0 bottom-0 rounded-bl-xl rounded-tr-xl py-3 px-5 text-slate-700 z-20 grid gap-1 w-full">
-            <div className="items-center text-xs">
-              <p className="text-white">{normal_price}</p>
+            <div className="items-center">
+              <p className="text-white font-semibold">{title}</p>
             </div>
-            <div className="flex justify-between">
-              <p className="text-white">Rp. {normal_price}</p>
-              <p className="text-white">Disc {discount} </p>
+            <div className="flex justify-between text-sm">
+              <p className="text-white">{partner_name}</p>
+              {discount ? (
+                <div className="min-w-[120px] flex justify-end">
+                  <p className="absolute bottom-8 text-white line-through text-xs">
+                    Rp. {normal_price}
+                  </p>
+                  <p className="text-white font-semibold">Rp. {final_price}</p>
+                </div>
+              ) : (
+                <p className="text-white">Rp. {normal_price}</p>
+              )}
             </div>
           </div>
         </picture>
