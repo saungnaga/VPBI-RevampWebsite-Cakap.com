5394ace6d1f868b2c7c78ee44743a5573f108c53
anugrahp13 <anugrahprastyo13@gmail.com>
2024-12-12 20:25:47 +0700

    refactor[course-detail.tsx]: Adding Responsive and Separating File Types

--- src/components/organism/course-detail.tsx
+++ src/components/organism/course-detail.tsx
@@ -1,286 +1,290 @@
-'use client';
+"use client";
 
-import React, { useState } from 'react';
-import Image from 'next/image';
+import Image from "next/image";
+import { useState } from "react";
 
-type BuyButtonProps = {
-	text: string;
-	onClick: () => void;
+const decodeHtmlEntities = (str) => {
+  const textarea = document.createElement("textarea");
+  textarea.innerHTML = str;
+  return textarea.value;
 };
 
-// Buy Button
-const BuyButton: React.FC<BuyButtonProps> = ({ text, onClick }) => (
-	<button
-		className='bg-blue-700 text-white text-lg py-2 px-4 rounded-lg w-full hover:bg-blue-400'
-		onClick={onClick}
-		type='button'
-	>
-		{text}
-	</button>
-);
+export default function CourseDetail({ courseData, courseRating }) {
+  const [isAboutCourseExpanded, setIsAboutCourseExpanded] = useState(false);
+  const [isCurriculumExpanded, setIsCurriculumExpanded] = useState(false);
+  const [isFacilitiesExpanded, setIsFacilitiesExpanded] = useState(false);
+  const [isScheduleExpanded, setIsScheduleExpanded] = useState(false);
+  const [isTermAndConditionExpanded, setIsTermAndConditionExpanded] =
+    useState(false);
+  const [activeTab, setActiveTab] = useState("Beli Kursus");
 
-// Need Refactor
-// Reausable Component Course Detail Content
-type CourseDetailContentProps = {
-	title: string;
-	content?: string;
-	children?: React.ReactNode;
-	maxLength?: number;
-};
+  const toggleAboutCourseExpand = () => {
+    setIsAboutCourseExpanded(!isAboutCourseExpanded);
+  };
 
-const CourseDetailContent: React.FC<CourseDetailContentProps> = ({
-	title,
-	content,
-	children,
-	maxLength = 250,
-}) => {
-	const [isExpanded, setIsExpanded] = useState(false);
+  const toggleCurriculumExpand = () => {
+    setIsCurriculumExpanded(!isCurriculumExpanded);
+  };
 
-	const shouldTruncateContent = content && content.length > maxLength;
+  const toggleFacilitiesExpand = () => {
+    setIsFacilitiesExpanded(!isFacilitiesExpanded);
+  };
 
-	const toggleExpanded = () => setIsExpanded(!isExpanded);
+  const toggleScheduleExpand = () => {
+    setIsScheduleExpanded(!isScheduleExpanded);
+  };
 
-	const displayedContent =
-		shouldTruncateContent && !isExpanded
-			? `${content.slice(0, maxLength)}...`
-			: content;
+  const toggleTermAndConditionExpand = () => {
+    setIsTermAndConditionExpanded(!isTermAndConditionExpanded);
+  };
+  const renderSidebarContent = () => {
+    if (activeTab === "Beli Kursus") {
+      return (
+        <>
+          <div className="mb-6">
+            <h3 className="text-lg font-semibold text-gray-900 mb-2">
+              Apa yang Kamu Dapat
+            </h3>
+            <div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: isFacilitiesExpanded
+                    ? courseData.courseDescription.facilities
+                    : courseData.courseDescription.facilities.substring(
+                        0,
+                        200
+                      ) +
+                      (courseData.courseDescription.facilities.length > 200
+                        ? "..."
+                        : ""),
+                }}
+              />
+              {courseData.courseDescription.facilities.length > 200 && (
+                <button
+                  onClick={toggleFacilitiesExpand}
+                  className="text-blue-500 font-medium hover:underline mt-2"
+                >
+                  {isFacilitiesExpanded ? "Sembunyikan" : "Baca Selengkapnya"}
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="mb-6">
+            <h3 className="text-lg font-semibold text-gray-900 mb-2">Jadwal</h3>
+            <div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: isScheduleExpanded
+                    ? courseData.courseDescription.schedule
+                    : courseData.courseDescription.schedule.substring(0, 200) +
+                      (courseData.courseDescription.schedule.length > 200
+                        ? "..."
+                        : ""),
+                }}
+              />
+              {courseData.courseDescription.schedule.length > 200 && (
+                <button
+                  onClick={toggleScheduleExpand}
+                  className="text-blue-500 font-medium hover:underline mt-2"
+                >
+                  {isScheduleExpanded ? "Sembunyikan" : "Baca Selengkapnya"}
+                </button>
+              )}
+            </div>
+          </div>
+          <div className="mb-6">
+            <h3 className="text-lg font-semibold text-gray-900 mb-2">Harga</h3>
+            <ul className="text-gray-700 text-base font-bold space-y-2">{`Rp ${Number(
+              courseData.finalPrice
+            ).toLocaleString("id-ID")}`}</ul>
+          </div>
+        </>
+      );
+    }
 
-	return (
-		<div className='grid gap-2'>
-			<h3 className='font-bold text-xl'>{title}</h3>
-			{displayedContent && (
-				<p
-					dangerouslySetInnerHTML={{ __html: displayedContent }}
-					className='text-gray-700'
-				/>
-			)}
-			{shouldTruncateContent && (
-				<button
-					className={`flex font-bold text-sm transition-all ${
-						isExpanded
-							? 'text-gray-600 justify-start'
-							: 'text-blue-500 justify-end'
-					}`}
-					onClick={toggleExpanded}
-				>
-					{isExpanded ? 'Sembunyikan' : 'Baca selengkapnya'}
-				</button>
-			)}
-			{children}
+    if (activeTab === "S&K") {
+      return (
+        <div className="mb-6">
+          <h3 className="text-lg font-semibold text-gray-900 mb-2">
+            Syarat & Ketentuan
+          </h3>
+          <div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: isTermAndConditionExpanded
+                  ? courseData.courseDescription.termAndCondition
+                  : courseData.courseDescription.termAndCondition.substring(
+                      0,
+                      200
+                    ) +
+                    (courseData.courseDescription.termAndCondition.length > 200
+                      ? "..."
+                      : ""),
+              }}
+            />
+            {courseData.courseDescription.termAndCondition.length > 200 && (
+              <button
+                onClick={toggleTermAndConditionExpand}
+                className="text-blue-500 font-medium hover:underline mt-2"
+              >
+                {isTermAndConditionExpanded
+                  ? "Sembunyikan"
+                  : "Baca Selengkapnya"}
+              </button>
+            )}
+          </div>
+        </div>
+      );
+    }
+  };
 
-			<hr />
-		</div>
-	);
-};
-
-// Tab
-type BuyCourseTabProps = {
-	facilities: string;
-	schedule: string;
-	price: string;
-	onBuy: () => void;
-};
-
-const BuyCourseTab: React.FC<BuyCourseTabProps> = ({
-	facilities,
-	schedule,
-	price,
-	onBuy,
-}) => (
-	<div className='grid gap-4'>
-		<CourseDetailContent title='Apa yang Kamu Dapat' content={facilities} />
-		<CourseDetailContent title='Jadwal' content={schedule} />
-		<CourseDetailContent title='Harga' content={price} />
-		<BuyButton text='Beli Kursus' onClick={onBuy} />
-	</div>
-);
-
-type TermAndConditionTabProps = {
-	tncs: string;
-};
-
-const TermAndConditionTab: React.FC<TermAndConditionTabProps> = ({ tncs }) => (
-	<div className='grid gap-4'>
-		<CourseDetailContent title='Syarat dan Ketentuan' content={tncs} />
-	</div>
-);
-
-// Need Refactor
-// Sticky Section Component
-type StickySectionProps = {
-	facilities: string;
-	schedule: string;
-	price: string;
-	tncs: string;
-	onBuy: () => void;
-};
-
-const StickySection: React.FC<StickySectionProps> = ({
-	facilities,
-	schedule,
-	price,
-	tncs,
-	onBuy,
-}) => {
-	const [activeTab, setActiveTab] = useState<'buy' | 'tnc'>('buy');
-
-	return (
-		<div className='grid border rounded-xl px-6 py-6 gap-6 sticky top-24'>
-			{/* Tab Navigation */}
-			<div className='grid grid-cols-2 border-b'>
-				<button
-					className={`px-4 py-2 font-semibold ${
-						activeTab === 'buy' ? 'border-b-2 border-blue-500' : ''
-					}`}
-					onClick={() => setActiveTab('buy')}
-				>
-					Beli Kursus
-				</button>
-				<button
-					className={`px-4 py-2 font-semibold ${
-						activeTab === 'tnc' ? 'border-b-2 border-blue-500' : ''
-					}`}
-					onClick={() => setActiveTab('tnc')}
-				>
-					S&K
-				</button>
-			</div>
-
-			{/* Tab Content */}
-			<div>
-				{activeTab === 'buy' && (
-					<BuyCourseTab
-						facilities={facilities}
-						schedule={schedule}
-						price={price}
-						onBuy={onBuy}
-					/>
-				)}
-				{activeTab === 'tnc' && <TermAndConditionTab tncs={tncs} />}
-			</div>
-		</div>
-	);
-};
-
-// Need Refactor
-type ReviewSectionProps = {
-	averageRating: number;
-	totalReviews: number;
-};
-
-const ReviewSection: React.FC<ReviewSectionProps> = ({
-	averageRating,
-	totalReviews,
-	// reviewList,
-}) => (
-	<div className='grid p-6 gap-4'>
-		<h3 className='text-xl font-bold'>Ulasan</h3>
-		<div className='rounded-lg border px-4 py-4'>
-			<p className='text-4xl'>{averageRating}</p>
-			<p className='text-gray-500'>{totalReviews} Ulasan</p>
-		</div>
-	</div>
-);
-
-type CourseDetailProps = {
-	courseData: any;
-	courseRating: any;
-};
-
-const CourseDetail: React.FC<CourseDetailProps> = ({
-	courseData,
-	courseRating,
-}) => {
-	return (
-		<div className='container bg-white text-black mx-auto p-6'>
-			{courseData && (
-				<>
-					<div className='grid grid-cols-3 gap-4'>
-						<div className='col-span-2'>
-							<div className='grid gap-6 p-6'>
-								<div className='grid gap-2'>
-									<p>{courseData.categoriesName}</p>
-									<h2 className='text-2xl font-bold'>
-										{courseData.courseName}
-									</h2>
-									<div className='flex gap-2'>
-										<p className='text-yellow-500 font-semibold'>
-											Rating: {courseData.rating}
-										</p>
-
-										<p className='text-gray-500 font-semibold'>
-											({courseRating.totalReviewer}{' '}
-											Ulasan)
-										</p>
-									</div>
-								</div>
-								<div className='w-full'>
-									<Image
-										src={courseData.icon.large}
-										width={1000}
-										height={200}
-										alt='Course Image'
-									/>
-								</div>
-								<div className='flex gap-4 border-b pb-2 items-center'>
-									<div className='align-middle'>
-										<Image
-											src={
-												courseData.partner.partnerLogo
-													.square
-											}
-											alt='Partner Logo'
-											width={50}
-											height={50}
-										/>
-									</div>
-									<div className='grid gap-2'>
-										<h3 className='text-xl font-bold'>
-											{courseData.partner.partnerName}
-										</h3>
-										<h4 className='text-sm font-bold text-gray-600'>
-											{courseData.tutorName}
-										</h4>
-									</div>
-								</div>
-								<CourseDetailContent
-									title='Tentang Kursus'
-									content={
-										courseData.courseDescription.aboutCourse
-									}
-								/>
-								<CourseDetailContent
-									title='Kurikulum'
-									content={
-										courseData.courseDescription.curriculum
-									}
-								/>
-							</div>
-							<ReviewSection
-								averageRating={courseRating.avgRating}
-								totalReviews={courseRating.totalReviewer}
-							/>
-						</div>
-						<div>
-							<StickySection
-								facilities={
-									courseData.courseDescription.facilities
-								}
-								schedule={courseData.courseDescription.schedule}
-								price={courseData.finalPrice}
-								tncs={
-									courseData.courseDescription
-										.termAndCondition
-								}
-								//
-								onBuy={() => alert('Membeli kursus!')}
-							/>
-						</div>
-					</div>
-				</>
-			)}
-		</div>
-	);
-};
-
-export default CourseDetail;
+  return (
+    <>
+      <div className="container mx-auto py-6">
+        {courseData && (
+          <div className="w-full">
+            <div className="flex flex-wrap justify-center">
+              <div className="w-full lg:w-3/4 mb-4 lg:mb-0 grid gap-4">
+                <div className="overflow-hidden border rounded-lg bg-white shadow-lg p-4">
+                  <div className="grid gap-1">
+                    <p className="text-dark text-sm dark:text-white md:mt-0">
+                      {courseData.categoriesName}
+                    </p>
+                    <h2 className="font-bold text-dark text-3xl dark:text-white md:mt-0">
+                      {courseData.courseName}
+                    </h2>
+                    <p className="flex justify-end text-dark text-sm dark:text-white md:mt-0">
+                      Rating : {""}
+                      <span className="font-semibold">
+                        {courseData.rating} / 5.0 | (
+                        {courseRating?.totalReviewer || 0} Ulasan )
+                      </span>
+                    </p>
+                    <Image
+                      src={courseData.icon.large}
+                      alt={courseData.courseName}
+                      width={1000}
+                      height={500}
+                    />
+                  </div>
+                  <div className="flex gap-2 my-3">
+                    <Image
+                      src={courseData.partner.partnerLogo.square}
+                      alt="Partner Logo"
+                      width={50}
+                      height={50}
+                    />
+                    <div className="flex flex-col">
+                      <div className="font-bold text-lg">
+                        {courseData.partner.partnerName}
+                      </div>
+                      <div className="text-sm">{courseData.tutorName}</div>
+                    </div>
+                  </div>
+                  <hr className="border-gray-300" />
+                  <div className="grid gap-4 my-4">
+                    <div className="flex flex-wrap gap-3">
+                      <div className="font-bold text-xl">Tentang Kursus</div>
+                      <div>
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: isAboutCourseExpanded
+                              ? courseData.courseDescription.aboutCourse
+                              : courseData.courseDescription.aboutCourse.substring(
+                                  0,
+                                  200
+                                ) +
+                                (courseData.courseDescription.aboutCourse
+                                  .length > 200
+                                  ? "..."
+                                  : ""),
+                          }}
+                        />
+                        {courseData.courseDescription.aboutCourse.length >
+                          200 && (
+                          <button
+                            onClick={toggleAboutCourseExpand}
+                            className="text-blue-500 font-medium hover:underline mt-2"
+                          >
+                            {isAboutCourseExpanded
+                              ? "Sembunyikan"
+                              : "Baca Selengkapnya"}
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                    <hr className="border-gray-300" />
+                    <div className="flex flex-col gap-3">
+                      <div className="font-bold text-xl">Kurikulum</div>
+                      <div>
+                        <div
+                          dangerouslySetInnerHTML={{
+                            __html: isCurriculumExpanded
+                              ? courseData.courseDescription.curriculum
+                              : courseData.courseDescription.curriculum.substring(
+                                  0,
+                                  200
+                                ) +
+                                (courseData.courseDescription.curriculum
+                                  .length > 200
+                                  ? "..."
+                                  : ""),
+                          }}
+                        />
+                        {courseData.courseDescription.curriculum.length >
+                          200 && (
+                          <button
+                            onClick={toggleCurriculumExpand}
+                            className="text-blue-500 font-medium hover:underline mt-2"
+                          >
+                            {isCurriculumExpanded
+                              ? "Sembunyikan"
+                              : "Baca Selengkapnya"}
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="overflow-hidden border rounded-lg bg-white shadow-lg p-4"></div>
+              </div>
+              <div className="w-full lg:w-1/4 ">
+                <aside className="w-full border rounded-lg bg-white shadow-lg p-4 sticky top-20 mr-0 lg:ml-6 mt-6 lg:mt-0 grid">
+                  <div className="flex border-b mb-4">
+                    <button
+                      className={`flex-1 py-2 text-center font-semibold ${
+                        activeTab === "Beli Kursus"
+                          ? "border-b-2 border-blue-500 text-blue-500"
+                          : "text-gray-500 hover:text-blue-500"
+                      }`}
+                      onClick={() => setActiveTab("Beli Kursus")}
+                    >
+                      Beli Kursus
+                    </button>
+                    <button
+                      className={`flex-1 py-2 text-center font-semibold ${
+                        activeTab === "S&K"
+                          ? "border-b-2 border-blue-500 text-blue-500"
+                          : "text-gray-500 hover:text-blue-500"
+                      }`}
+                      onClick={() => setActiveTab("S&K")}
+                    >
+                      S&K
+                    </button>
+                  </div>
+                  {renderSidebarContent()}
+                  <button
+                    onClick={() => alert("Proses beli barang dimulai!")}
+                    className="mt-6 py-3 px-6 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+                  >
+                    Beli Barang
+                  </button>
+                </aside>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
